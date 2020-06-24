5c87e339fee317948afdbc1ea79c2a5cb9e86f42
Tsubasa.Yamazaki <67271578+283cat@users.noreply.github.com>
2020-06-24 15:09:37 +0900

    入力した内容をHTML上に表示する機能の実装

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -13,11 +12,6 @@ func main() {
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
-	type data struct {
-		importance string `json:"importance"`
-    	task       string `json:"task"`
-    	limit      string `json:"limit"`
-	}
 
 	fmt.Println("Hallo world!")
 }
--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,23 @@
 
 
-function entry() {
+function entry(id) {
 	
-	fetch("/post");
+	let imp = document.post.impName.value;
+	let task = document.post.taskName.value;
+	let limit = document.post.limitName.value;
+	
+	let table = document.getElementById(id);
+
+	let newRow   = table.insertRow(-1);
+  
+	let cell1 = newRow.insertCell(-1);
+	let cell2 = newRow.insertCell(-1);
+	let cell3 = newRow.insertCell(-1);
+	let newText1  = document.createTextNode(imp);
+	let newText2  = document.createTextNode(task);
+	let newText3  = document.createTextNode(limit);
+
+	cell1.appendChild(newText1);
+	cell2.appendChild(newText2);
+	cell3.appendChild(newText3);
 }
