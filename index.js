b84f0434e13ebd52ad4bd9d2f3744e1c265bd619
Tsubasa.Yamazaki <67271578+283cat@users.noreply.github.com>
2020-06-26 21:02:13 +0900

    DB搭載バージョン

---
+++ index.js
@@ -0,0 +1,103 @@
+function entry() {
+	
+	let impElement = document.getElementById("post");
+	let impRadio = impElement.imp;
+	let importance = impRadio;
+	let task = document.getElementById("task");
+	let deadline = document.getElementById("deadline");
+
+	let todoData = {
+		importance: importance.value,
+		task: task.value,
+		deadline: deadline.value,
+	}
+
+    fetch("/post", {
+		method : "POST",
+		headers : {
+			'Content-Type': 'application/json'
+		},
+        body : JSON.stringify(todoData),
+	}).then((response)=>{
+		display();
+	})
+	alert("登録しました")
+
+}
+
+function display () {
+	fetch("/todoList", {
+		method : "GET",
+		headers : {
+			'Content-Type': 'application/json'
+		},
+	}).then(response => { 
+		return response.json()
+	}).then(todoData => {
+		tableDelete();
+		displayTable(todoData);
+	});
+}
+
+function tableDelete() {
+	let table = document.getElementById(`table`);
+
+	let loopCount = table.rows.length;
+	for(let i = 0 ; i < loopCount; i++) {
+		if (i === 0) {
+			continue;
+		}
+		table.deleteRow(1);
+	}
+}
+
+function displayTable(todoData) {
+
+	todoData.forEach(function(element) {
+
+		let table = document.getElementById(`table`);
+        let id = element.id;
+		let imp = element.importance;
+		let task = element.task;
+        let limit = element.deadline;
+		let newRow  = table.insertRow(-1);
+  
+		let cell1 = newRow.insertCell(-1);
+		let cell2 = newRow.insertCell(-1);
+		let cell3 = newRow.insertCell(-1);
+		let cell4 = newRow.insertCell(-1);
+	
+		let impText  = document.createTextNode(imp);
+		let taskText  = document.createTextNode(task);
+		let limitText  = document.createTextNode(limit);
+	
+		cell1.appendChild(impText);
+		cell2.appendChild(taskText);
+		cell3.appendChild(limitText);
+
+		let dButton = document.createElement("button");
+        dButton.type = "button";
+        dButton.value = id;
+		dButton.innerText = "削除";
+		dButton.addEventListener("click", deleteMode, false);
+		cell4.appendChild(dButton);
+        });
+}
+
+function deleteMode() {
+
+	let todoData = {
+		id: parseInt(this.value, 10)
+	}
+
+	fetch("/todoDelete", {
+		method : "POST",
+		headers : {
+			'Content-Type': 'application/json'
+		},
+        body : JSON.stringify(todoData),
+	}).then((response)=>{
+		display();
+	})
+	alert("削除しました")
+}
--- main.go
+++ main.go
@@ -1,17 +1,98 @@
 package main
 
 import (
+	"log"
 	"fmt"
 	"net/http"
+	"database/sql"
+	"encoding/json"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
+type todo struct {
+	Id        	int    `json:"id"`
+	Importance  string `json:"importance"`
+	Task		string `json:"task"`
+	Deadline 	string `json:"deadline"`
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("static")))
+	var err error
+	db, err = sql.Open("mysql", "root:0111@/todo283")
+	if err != nil {
+		return
+	}
+	defer db.Close()
+
+	log.Println("Open the 283todo.")
+
+	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.HandleFunc("/post", post)
+	http.HandleFunc("/todoList", todoList)
+	http.HandleFunc("/todoDelete", todoDelete)
 	http.ListenAndServe(":8080", nil)
 }
 
+// 登録
 func post(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Println("Hallo world!")
+	var body todo
+	err := json.NewDecoder(r.Body).Decode(&body)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+	tdl, err := db.Prepare("INSERT todolist SET importance=?,task=?,deadline=?")
+    if err != nil {
+		return
+	}
+    _, err = tdl.Exec(body.Importance, body.Task, body.Deadline)
+    if err != nil {
+		return
+	}
 }
+
+// 表示
+func todoList(w http.ResponseWriter, r *http.Request) {
+	var todos []todo
+	var err error
+	rows, err := db.Query("SELECT * FROM todolist")
+    if err != nil {
+        return
+	}
+	
+	fmt.Println(rows)
+    for rows.Next() {
+        var todotable todo
+        err = rows.Scan(&todotable.Id, &todotable.Importance, &todotable.Task, &todotable.Deadline)
+  
+		todos = append(todos, todotable)
+    }
+	todoJson, err := json.Marshal(todos)
+    if err != nil {
+        return
+	}
+
+	w.Write(todoJson)
+}
+
+// 削除
+func todoDelete(w http.ResponseWriter, r *http.Request) {
+	var body todo
+	err := json.NewDecoder(r.Body).Decode(&body)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+	}
+	deleteId := body.Id
+	tdl, err := db.Prepare("DELETE FROM todolist WHERE id = ?")
+    if err != nil {
+		return
+	}
+    _, err = tdl.Exec(deleteId)
+    if err != nil {
+		return
+	}
+}
+
