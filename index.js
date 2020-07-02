c2762742071e4d7a55cf90910640934b8e321370
TsubasaYamazaki <tsubasa_yamazaki@egxp.io>
2020-07-02 16:03:03 +0900

    vue.js導入

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = new Vue({
     importance: "",
     task: "",
     deadline: "",
+
+    todos: []
   },
 
   created: function () {
@@ -43,59 +45,14 @@ const app = new Vue({
           return response.json();
         })
         .then((todoData) => {
-          this.tableDelete();
-          this.displayTable(todoData);
+          this.todos = todoData;
         });
     },
 
-    //画面テーブル削除
-    tableDelete: function () {
-      let table = document.getElementById(`table`);
-      let loopCount = table.rows.length;
-
-      for (let i = 0; i < loopCount; i++) {
-        if (i === 0) {
-          continue;
-        }
-        table.deleteRow(1);
-      }
-    },
-
-    //画面テーブル生成
-    displayTable: function (todoData) {
-      todoData.forEach(function (element) {
-        let table = document.getElementById(`table`);
-        let id = element.id;
-        let imp = element.importance;
-        let task = element.task;
-        let limit = element.deadline;
-
-        let newRow = table.insertRow(-1);
-        let cell1 = newRow.insertCell(-1);
-        let cell2 = newRow.insertCell(-1);
-        let cell3 = newRow.insertCell(-1);
-        let cell4 = newRow.insertCell(-1);
-        let impText = document.createTextNode(imp);
-        let taskText = document.createTextNode(task);
-        let limitText = document.createTextNode(limit);
-
-        cell1.appendChild(impText);
-        cell2.appendChild(taskText);
-        cell3.appendChild(limitText);
-
-        let dButton = document.createElement("button");
-        dButton.type = "button";
-        dButton.value = id;
-        dButton.innerText = "削除";
-        dButton.addEventListener("@click", this.deleteMode, false);
-        cell4.appendChild(dButton);
-      });
-    },
-
     //削除ボタン
-    deleteMode: function () {
+    deleteMode: function (id) {
       let todoData = {
-        id: parseInt(this.value, 10),
+        id: id,
       };
 
       fetch("/todoDelete", {
